03a2e4e37a77f228e8a49fee342a6ca07049b7ee
rafahuelin <rafahuelin@gmail.com>
2021-12-19 22:59:29 +0100

    create interactive d3 chart

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,51 @@
 <template>
   <div id="app">
-    <Chart :coupons="coupons" />
+    <h1>Data visualization</h1>
+    <Chart
+      :axisData="axisData"
+      :coupons="coupons"
+      title="Tickets vs. Value"
+      :companies="companies"
+    />
   </div>
 </template>
 
 <script>
-import Chart from './components/Chart.vue'
-import { coupons } from './assets/json/coupons.json'
-
-console.log(coupons)
+import Chart from "./components/Chart.vue";
+import { coupons } from "./assets/json/coupons.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Chart
+    Chart,
   },
   data() {
     return {
-      coupons
-    }
+      coupons,
+      axisData: coupons
+                  .filter(el => el.promotion_type === 'percent-off')
+                  .map(el => ({x: el.coupon_id, y: el.value, company: el.webshop_id}))
+    };
+  },
+  computed: {
+    companies() {
+      return [...new Set(this.coupons.map(el => el.webshop_id))]
+    },
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: aquamarine;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: navy;
   margin-top: 60px;
 }
 </style>
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,16 +1,120 @@
 <template>
   <div>
-    {{coupons[0]}}
+    <h3>{{title}}</h3>
+    <select v-model="selected" >
+      <option v-for="company in companies" :key="company" :value="company">{{titleCase(company)}}</option>
+    </select>
+    <br />
+    <svg></svg>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3"
+
 export default {
   name: 'Chart',
-  props: ['coupons']
+  props: ['axisData', 'companies', 'title'],
+  data() {
+    return {
+      selected: this.companies[0],
+    }
+  },
+  computed: {
+    filteredData() {
+      return this.axisData.filter(el => el.company === this.selected)
+    },
+    maxValue() {
+      return Math.max(...this.filteredData.map(el => el.y))
+    },
+  },
+  methods: {
+    titleCase(title) {
+      return title.charAt(0).toUpperCase() + title.slice(1)
+    },
+    renderChart() {
+      const height = 300
+      const roundedHeight = Math.ceil((height + 1)/10) * 10
+      const width = 800
+
+      // set the ranges
+      const xScale = d3
+        .scaleBand()
+        .domain(this.filteredData.map(dataPoint => dataPoint.x))
+        .range([0, width])
+        .padding(0.2)
+
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, this.maxValue])
+        .range([roundedHeight, 10])
+
+      const container = d3.select('svg')
+        .classed('chart-container', true)
+        .style('height', `${roundedHeight}px`)
+        .style('width', `${width}px`)
+
+      // eslint-disable-next-line no-unused-vars
+      const bar = container
+        .selectAll('.bar')
+        .data(this.filteredData)
+        .enter()
+        .append('rect')
+        .classed('bar', true)
+        .attr('width', xScale.bandwidth())
+        .attr('height', data => roundedHeight - yScale(data.y))
+        .attr('x', data => (xScale(data.x)))
+        .attr('y', data => yScale(data.y))
+
+      // add the x Axis
+      container.append("g")
+        .attr('transform', "translate(0," + roundedHeight + ")")
+        .call(d3.axisBottom(xScale))
+        .selectAll('text')
+        .attr('transform', "translate(-15, 15) rotate(-45)");
+
+      // add the y Axis
+      container.append("g")
+        .call(d3.axisLeft(yScale));
+    }
+  },
+  mounted() {
+    this.renderChart()
+  },
+  updated() {
+    this.renderChart()
+  },
+  beforeUpdate() {
+    var svg = d3.select("svg")
+    svg
+      .selectAll('*')
+      .remove()
+  },
 }
 </script>
 
 <style>
+  .chart-container {
+    border: 1px solid navy;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: visible;
+  }
 
+  .bar { 
+    fill: rgb(255, 0, 212);
+  }
+
+  text {
+    fill: navy;
+    font-size: smaller;
+  }
+
+  path.domain {
+    stroke: transparent;
+  }
+
+  select {
+    margin-bottom: 20px;
+  }
 </style>
